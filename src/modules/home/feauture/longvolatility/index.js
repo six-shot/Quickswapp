ccbbda63b87c258c5ff242919dcc022f77b2f174
six-shot <okhuomonajayi54@gmail.com>
2024-03-22 09:18:22 +0000

    first commit

--- src/modules/home/feauture/longvolatility/index.js
+++ src/modules/home/feauture/longvolatility/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { TypingText } from "../../utils/customtext";
+
 import { motion } from "framer-motion";
+import { TypingText } from "../../utils/customtext";
+import { staggerContainer, fadeIn } from "../../utils/motion";
 import rocket from "../../assets/3dicons.png";
 import dollar from "../../assets/dollar.png";
 import coins from "../../assets/coins.png";
-import { staggerContainer, fadeIn } from "../../utils/motion";
-
+import { Button } from "../../components/ui/buttons/Button";
 export default function LongVolatilty() {
   return (
     <motion.div
@@ -13,12 +14,38 @@ export default function LongVolatilty() {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className="bg-[#090B18] h-screen flex justify-center items-center relative z-10 overflow-hidden"
+      className="w-full  bg-[#090B18] relative z-10 py-[10%]"
     >
-      <div className="max-w-[1440px] mx-auto w-full">
-        <div className="  bg-[#FF630B] z-10 absolute w-[400px] h-[400px] rounded-[400px] blur-[150px]  bottom-0 left-0" />
-        <div className="grid sm:grid-cols-2 grid-cols-1 items-center  px-[4%]  w-full">
-          <div className="relative col-span-1 ">
+      <div className="max-w-[1440px] mx-auto">
+        <div className="  relative z-10 grid sm:grid-cols-2 grid-cols-1 space-y-32  items-center justify-center  py-14 sm:h-screen h-min px-[4%]">
+          <div className="col-span-1">
+            <motion.div
+              variants={fadeIn("left", "tween", 0.2, 1)}
+              className="flex  flex-col items-start text-[#FAFAFACC] flex-1 gap-4"
+            >
+              <div className=" text-2xl ">
+                <TypingText title="Liquidity Provisioning" />
+              </div>
+              <h2 className="sm:text-[48px] text-2xl font-extrabold leading-[1.1]">
+                Earn by Providing <br className="sm:flex hidden" /> Liquidity
+              </h2>
+
+              <p className="sm:text-lg text-base">
+                Earn a higher yield by providing liquidity through GammaSwap.
+                <br className="sm:flex hidden" />
+                Think of our wrapped pools like wrapped tokens.
+              </p>
+              <p className="sm:text-lg text-base mt-6 mb-3">
+                The GammaSwap protocol will directly deposit liquidity into{" "}
+                <br className="sm:flex hidden" />
+                the underlying AMM and provide an LP token to the depositor
+                <br className="sm:flex hidden" />
+                representing the position.
+              </p>
+              <Button variant="secondary">Learn More </Button>
+            </motion.div>
+          </div>
+          <div className=" col-span-1 sm:mt-0 ">
             <motion.div
               style={{ position: "absolute", top: "-15%", left: "5%" }}
             ></motion.div>
@@ -71,32 +98,6 @@ export default function LongVolatilty() {
                 alt="dollar"
                 className="absolute bottom-0 right-[15%]"
               />
-            </motion.div>
-          </div>
-          <div className="col-span-1 order-first sm:order-last ">
-            <motion.div
-              variants={fadeIn("right", "tween", 0.2, 1)}
-              className="flex flex-col text-[#FAFAFACC]"
-            >
-              <div className=" text-2xl ">
-                <TypingText title="Perpetuals" />
-              </div>
-              <h2 className="text-[48px] font-extrabold leading-[1.1]">
-                Long Volatility
-              </h2>
-              <p className="text-lg">
-                Turn Impermanent Loss into Impermanent Gain. Get perpetaul
-                <br />
-                volatility exposure on any asset in the form of a long, short or
-                <br />
-                straddle. Unlike perpetual futures, liquidations are time
-                <br />
-                dependent - protecting traders from unexpected price movements.
-              </p>
-              <p className="text-lg mt-6">
-                GammaSwap perpetuals are fully collateralized offering<br/>
-                non-linear returns that increase as price changes.
-              </p>
             </motion.div>
           </div>
         </div>
