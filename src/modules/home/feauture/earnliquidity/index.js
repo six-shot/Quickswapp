62e6bcb706a2d74131512a935213665105fc13bb
six-shot <okhuomonajayi54@gmail.com>
2024-03-22 10:15:25 +0000

    hero

--- src/modules/home/feauture/earnliquidity/index.js
+++ src/modules/home/feauture/earnliquidity/index.js
@@ -4,6 +4,7 @@ import bg from "../../assets/earn.png";
 import { motion } from "framer-motion";
 import { TypingText } from "../../utils/customtext";
 import { staggerContainer, fadeIn, planetVariants } from "../../utils/motion";
+import { Button } from "../../components/ui/buttons/Button";
 export default function EarnLiquidity() {
   return (
     <motion.div
@@ -11,39 +12,45 @@ export default function EarnLiquidity() {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className="bg-[#090B18]  h-screen     relative z-10 sm:py-[2%] py-0"
+      className="w-full mt-[1.5%] bg-[#090B18] relative z-10 sm:py-[2%] py-0"
     >
       <div className="max-w-[1440px] mx-auto">
-        <div className="grid sm:grid-cols-2 h-full grid-cols-1 w-full  space-y-20 items-center sm:px-[4%] px-[6%]">
-          <div className="col-span-1 w-full ">
+        <div className="  relative z-10 grid sm:grid-cols-2 grid-cols-1 space-y-32  items-center justify-center  py-14 sm:h-screen h-min px-[4%]">
+          <div className="col-span-1">
             <motion.div
               variants={fadeIn("left", "tween", 0.2, 1)}
-              className="flex  flex-col text-[#FAFAFACC] flex-1 gap-4"
+              className="flex  flex-col items-start text-[#FAFAFACC] flex-1 gap-4"
             >
               <div className=" text-2xl ">
-                <TypingText title="Multiple AMMs" />
+                <TypingText title="Liquidity Provisioning" />
               </div>
-              <h2 className="text-[48px] font-extrabold leading-[1.1]">
-                Earn by Providing <br /> Liquidity
+              <h2 className="sm:text-[48px] text-2xl font-extrabold leading-[1.1]">
+                Earn by Providing <br className="sm:flex hidden" /> Liquidity
               </h2>
 
-              <p className="text-lg">
+              <p className="sm:text-lg text-base">
                 Earn a higher yield by providing liquidity through GammaSwap.
-                <br />
+                <br className="sm:flex hidden" />
                 Think of our wrapped pools like wrapped tokens.
               </p>
-              <p className="text-lg mt-6">
+              <p className="sm:text-lg text-base mt-6 mb-3">
                 The GammaSwap protocol will directly deposit liquidity into{" "}
-                <br /> the underlying AMM and provide an LP token to the
-                depositor
-                <br />
+                <br className="sm:flex hidden" />
+                the underlying AMM and provide an LP token to the depositor
+                <br className="sm:flex hidden" />
                 representing the position.
               </p>
+              <Button variant="secondary">Learn More </Button>
             </motion.div>
           </div>
-          <div className="col-span-1 w-full">
-            <motion.div className="relative  ">
-              <motion.img className="rotate" src={earn} alt="dollar" />
+          <div className=" col-span-1 sm:mt-0 w-full">
+            <motion.div className="flex jutify-center items-center  ">
+              <motion.img
+
+                src={earn}
+                alt="dollar"
+              />
+
             </motion.div>
           </div>
         </div>
